223396e5c8a2d1540ec5cc399a4742fc4132c818
ballingerj <ballingerj@allegheny.edu>
2016-07-06 12:55:44 -0400

    Added support for doubles

--- fun_stuff/factorial/factorial.java
+++ fun_stuff/factorial/factorial.java
@@ -1,12 +1,24 @@
 public class factorial {
   public static void main(String args[]) {
-    System.out.println(factorial(Integer.parseInt(args[0])));
+    try {
+        double d = Double.parseDouble(args[0]);
+        if (d % 1.0 != 0) {
+          System.out.println("You must input an integer!");
+          System.exit(0);
+        } // if
+        d = factorial(d);
+        int i = (int) d;
+        System.out.println(i);
+    } catch (Exception e) {
+      System.out.println("You must input as an interger!");
+      e.printStackTrace();
+    } // try-catch
   } // main
 
-  public static int factorial(int a) {
-    if (a == 1) {
+  public static double factorial(double a) {
+    if (a == 1.0) {
       return a;
     }
-    return a * factorial(a-1);
-  }
+    return a * factorial(a-1.0);
+  } // factorial
 } // main
